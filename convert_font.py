10f4e71781b8064193b47fe8e4f8b0746d919e72
Grimm-N <natali.grimm.mim@gmail.com>
2024-10-29 00:33:53 +0200

    python update

--- convert_font.py
+++ convert_font.py
@@ -1,13 +1,15 @@
 from fontTools.ttLib import TTFont
 
-input_file = "C:/Users/natal/OneDrive/Desktop/Challenge/base-apparel-coming-soon/fonts/JosefinSans-VariableFont_wght.ttf"
+input_file = "./fonts/JosefinSans-VariableFont_wght.ttf"
 
 font = TTFont(input_file)
 
-output_file_woff = "C:/Users/natal/OneDrive/Desktop/Challenge/base-apparel-coming-soon/fonts/JosefinSans-VariableFont_wght.woff"
+output_file_woff = "./fonts/JosefinSans-VariableFont_wght.woff"
+font.flavor = 'woff'
 font.save(output_file_woff)
 
-output_file_woff2 = "C:/Users/natal/OneDrive/Desktop/Challenge/base-apparel-coming-soon/fonts/JosefinSans-VariableFont_wght.woff2"
+output_file_woff2 = "./fonts/JosefinSans-VariableFont_wght.woff2"
+font.flavor = 'woff2'
 font.save(output_file_woff2)
 
 print("Fonts converted successfully!")
